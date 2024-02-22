18d1d970dd4356ec81c47bb17d16f6fc5366cc76
PlazmaDolphin <plasmadolphin0@gmail.com>
2024-02-21 21:42:30 -0500

    Completed Vector2d class for Project 1

--- proj1/Vector2d.cpp
+++ proj1/Vector2d.cpp
@@ -6,35 +6,27 @@
 
 
 // Default constructor
-Vector2d::Vector2d() : x(0.0), y(0.0)
-{
-	// intentionally blank
-}
+Vector2d::Vector2d() : x(0.0), y(0.0){}
 
-Vector2d::Vector2d(double _x, double _y) : x(_x), y(_y)
-{
-	// intentionally blank
-}
+Vector2d::Vector2d(double _x, double _y) : x(_x), y(_y){}
 
 // Dot product
 double Vector2d::dot(const Vector2d& v) const
 {
-	// FIXME
-	return 0.0;
+	return x*v.x + y*v.y;
 }
 
 // Vector magnitude
 double Vector2d::magnitude() const
 {
-	// FIXME
-	return 0.0;
+	return sqrt(x*x+y*y);
 }
 
 // Nomralized vector
 const Vector2d Vector2d::normalize() const
 {
-	// FIXME
-	return Vector2d();
+	if(magnitude()==0) return *this;
+	return Vector2d(x/magnitude(), y/magnitude());
 }
 
 // getters and setters
@@ -50,62 +42,64 @@ double Vector2d::getY() const
 
 void Vector2d::setX(double _x)
 {
-	// FIXME
+	x=_x;
 }
 
 void Vector2d::setY(double _y)
 {
-	// FIXME
+	y=_y;
 }
 
 // overloaded operators -- assignment operators
 Vector2d& Vector2d::operator+=(const Vector2d& rhs)
 {
-	// FIXME
+	x+=rhs.x;
+	y+=rhs.y;
 	return *this;
 }
 
 Vector2d& Vector2d::operator-=(const Vector2d& rhs)
 {
-	// FIXME
+	x-=rhs.x;
+	y-=rhs.y;
 	return *this;
 }
 
 Vector2d& Vector2d::operator*=(double c)
 {
-	// FIXME
+	x*=c;
+	y*=c;
 	return *this;
 }
 
 // overloaded operators -- arithmetic
 const Vector2d operator +(Vector2d lhs, const Vector2d& rhs)
 {
-	// FIXME
-	return Vector2d();
+	lhs+=rhs;
+	return lhs;
 }
 
 const Vector2d operator -(Vector2d lhs, const Vector2d& rhs)
 {
-	// FIXME
-	return Vector2d();
+	lhs-=rhs;
+	return lhs;
 }
 
 const Vector2d operator *(Vector2d lhs, double rhs)
 {
-	// FIXME
-	return Vector2d();
+	lhs*=rhs;
+	return lhs;
 }
 
 const Vector2d operator *(double lhs, Vector2d rhs)
 {
-	// FIXME
-	return Vector2d();
+	rhs*=lhs;
+	return rhs;
 }
 
 const Vector2d Vector2d::operator-()
 {
-	// FIXME
-	return Vector2d();
+	return Vector2d(-x, -y);
 }
 
 
@@ -113,26 +107,25 @@ const Vector2d Vector2d::operator-()
 // overloaded operators -- comparisons
 bool operator ==(const Vector2d& v1, const Vector2d& v2)
 {
-	// FIXME
-	return false;
+	return v1.x==v2.x && v1.y==v2.y;
 }
 
 bool operator !=(const Vector2d& v1, const Vector2d& v2)
 {
-	// FIXME
-	return false;
+	return !(v1==v2);
 }
 
 // overloaded I/O operators
 std::ostream& operator <<(std::ostream& os, const Vector2d& v)
 {
-	// FIXME
+	os<<"("<<v.x<<", "<<v.y<<")";
 	return os;
 }
 
 std::istream& operator >> (std::istream& is, Vector2d& v)
 {
-	// FIXME
+	is >> v.x;
+	is >> v.y;
 	return is;
 }
 
